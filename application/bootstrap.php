aee4afeb883eabe7f4652a8552b5f15b3e7f0984
amen <mur_mail@ukr.net>
2015-09-27 15:29:48 +0300

    Installing Kohana framework

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -124,10 +124,10 @@ Kohana::modules(array(
 	// 'auth'       => MODPATH.'auth',       // Basic authentication
 	// 'cache'      => MODPATH.'cache',      // Caching with multiple backends
 	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
-	// 'database'   => MODPATH.'database',   // Database access
+	 //'database'   => MODPATH.'database',   // Database access
 	// 'image'      => MODPATH.'image',      // Image manipulation
 	// 'minion'     => MODPATH.'minion',     // CLI Tasks
-	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
+	 //'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 	));
@@ -139,7 +139,7 @@ Kohana::modules(array(
  * If you have not defined a cookie salt in your Cookie class then
  * uncomment the line below and define a preferrably long salt.
  */
-// Cookie::$salt = NULL;
+ Cookie::$salt = '@salt';
 
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
--- application/classes/Controller/Index.php
+++ application/classes/Controller/Index.php
@@ -1,19 +1,32 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Index extends Controller_Template {
+/*class Controller_Index extends Controller_Template {
 
-	public $template = 'v_index';
+	public $template = 'header';
 
 	public function before()
 	{
 		parent::before();
-		$this->companyName = "Logistic line LTD";
+		$this->companyName = "Logistic Line LTD";
 	}
 
 	public function action_index()
 	{
-		$welcomeText = Model::factory('Index')->getWelcomeText();
-		$this->template->title = $welcomeText;
+	}*/
+
+
+class Controller_Index extends Controller{
+
+	public function action_index()
+	{
+//		echo $this->request->response = View::factory('header');
+		echo View::factory('header');
+
+	}
+
+	public function action_custom()
+	{
+
 	}
 
 } // End Welcome
