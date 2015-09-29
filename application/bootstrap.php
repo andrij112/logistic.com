d5eaea46cf1243571a3cd646f528cd45fdcf4e06
amen <mur_mail@ukr.net>
2015-09-29 08:17:24 +0300

    Не працюють роути

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -103,7 +103,8 @@ if (isset($_SERVER['KOHANA_ENV']))
  * - boolean  expose      set the X-Powered-By header                        FALSE
  */
 Kohana::init(array(
-	'base_url'     => '/kohana/',
+//	'base_url'     => '/kohana/',
+	'base_url'     => '/',
 	'index_file'   => false ,
 ));
 
@@ -145,8 +146,17 @@ Kohana::modules(array(
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
+
+Route::set('custom', '(<controller>(/<action>(/<id>)))')
+	->defaults(array(
+		'controller' => 'index',
+		'action'     => 'custom',
+	));
+
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
 		'controller' => 'index',
 		'action'     => 'index',
 	));
+
+
--- application/classes/Controller/Index.php
+++ application/classes/Controller/Index.php
@@ -20,13 +20,16 @@ class Controller_Index extends Controller{
 	public function action_index()
 	{
 //		echo $this->request->response = View::factory('header');
+		echo Request::current()->action(); die;
 		echo View::factory('header');
 
 	}
 
 	public function action_custom()
 	{
-
+		echo URL::base();
+		echo URL::base('https', TRUE);
+//		echo View::factory('custom_form');
 	}
 
 } // End Welcome
