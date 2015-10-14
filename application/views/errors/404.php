83ccd41c44d422d49efdff5d16a4291d7483d343
andrij112 <mur_mail@ukr.net>
2015-10-15 00:02:37 +0300

    Сторінка помилки 404 (Тестовий вигляд)

--- application/views/errors/404.php
+++ application/views/errors/404.php
@@ -1,1 +1,89 @@
-<?php
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="ru">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8" />
+    <title>Logistic Line LTD</title>
+    <meta name="description"           content="Услуги в сфере логистики" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport"              content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet"     href="public/css/fonts.css" />
+    <link rel="stylesheet"     href="public/css/reset.css" />
+    <link rel="stylesheet"     href="public/css/main.css" />
+    <link rel="stylesheet"     href="public/css/jquery.bxslider.css" />
+    <link rel="stylesheet"     href="public/css/media.css" />
+</head>
+<body>
+<div id="top_background"></div>
+<div class="wrapper">
+    <header>
+        <div class="header_topline">
+            <div id="top_background_wrapper" class="default">
+                <img id="logo" src="public/img/logo.png" alt="logo"/>
+                <div id="main_label">
+                    <h1>Logistic Line LTD </h1>
+                </div>
+                <div id="main_menu">
+                    <ul>
+                        <li id="menu_button_main">
+                            <a href="/main">
+                                <span>Главная</span>
+                            </a>
+                        </li>
+                        <li id="menu_button_custom">
+                            <a href="/custom"><span>Заказ</span>
+                            </a>
+                        </li>
+                        <li id="menu_button_services">
+                            <a href="services">
+                                <span>Услуги</span>
+                            </a>
+                        </li>
+                        <li id="menu_button_information">
+                            <a href="information">
+                                <span>Информация</span>
+                            </a>
+                        </li>
+                        <li id="menu_button_contacts">
+                            <a href="contacts">
+                                <span>Контакты</span>
+                            </a>
+                        </li>
+                        <li id="menu_button_careers">
+                            <a href="careers">
+                                <span>Вакансии</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+<section>
+<div class="error_wrapper">
+
+</div>
+</section>
+<footer>
+    <div id="footer_company_label">© Logistic Line LTD</div>
+    <div id="footer_contacts">Тел.: (044) 22-11-824 e-mail: lll@ltd.ua Все права защищены</div>
+    <div id="footer_developed">Powered by <span class="red">A</span>.MUR <span class="red">S</span>TUDIO</div>
+</footer>
+</div>
+<!--    <div id="bottom_background"></div>-->
+<!--[if lt IE 9]>
+<script src="public/libs/html5shiv/es5-shim.min.js"></script>
+<script src="public/libs/html5shiv/html5shiv.min.js"></script>
+<script src="public/libs/html5shiv/html5shiv-printshiv.min.js"></script>
+<script src="public/libs/respond/respond.min.js"></script>
+<![endif]-->
+<script src="public/libs/jquery/jquery-1.11.1.min.js"></script>
+<script src="public/libs/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="public/js/common.js"></script>
+<script src="public/js/requests.js"></script>
+</body>
+</html>
